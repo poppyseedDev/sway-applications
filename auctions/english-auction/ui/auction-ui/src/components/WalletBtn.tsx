bc6831cfff8c8cd880cf9b6ba490f5470beff8f3
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-03-02 07:44:16 -0700

    adding ui, it works

--- auctions/english-auction/ui/auction-ui/src/components/WalletBtn.tsx
+++ auctions/english-auction/ui/auction-ui/src/components/WalletBtn.tsx
@@ -1,9 +1,34 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  useConnectUI,
+  useDisconnect,
+  useIsConnected,
+  useWallet,
+} from '@fuel-wallet/react';
+
 
 export const WalletBtn = () => {
+  const { connect, setTheme, isConnecting } = useConnectUI();
+  const { isConnected } = useIsConnected();
+  const { wallet } = useWallet();
+  const {disconnect } = useDisconnect();
+
   return (
     <div>
-      <ConnectButton />
+      <div >
+        {isConnected ? (
+          <>
+            <button onClick={() => disconnect()} className='bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded'>
+              {isConnecting ? 'Connecting' : 'Disconnect Wallet'}
+            </button>
+
+            {/* Add more buttons and inputs as needed for other actions like withdraw */}
+          </>
+        ) : (
+          <button onClick={() => connect()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded'>
+            {isConnecting ? 'Connecting' : 'Connect Wallet'}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
